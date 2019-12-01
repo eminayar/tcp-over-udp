5c9ba5f1c5edf195a323bcc1031496a241f8be50
eminayar <m.eminayar06@gmail.com>
2019-12-01 22:48:05 +0300

    aegawgawg

--- file.py
+++ file.py
@@ -8,14 +8,14 @@ import time
 
 os.system('clear')
 host_name = socket.gethostname()
-host_ip = "192.168.1.38"
+host_ip = "192.168.1.104"
 
 PORT = 12345
 
 ## [<PacketId>,<data>]
 class TCP:
     def __init__(self):
-        self.PACKET_SIZE = 1500
+        self.PACKET_SIZE = 5
         self.MAXID = 10000
         self.packetCounter = 0
         _thread.start_new_thread(self.receiver, () )
@@ -30,10 +30,10 @@ class TCP:
         sock.bind(('', PORT))
         sock.setblocking(False)
         while True:
-            # result = select.select([sock],[],[])
-            # msg = result[0][0].recv(self.PACKET_SIZE).decode('ascii')
+            result = select.select([sock],[],[])
+            msg = result[0][0].recv(self.PACKET_SIZE).decode('ascii')
             print(sock.getsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF))
-            time.sleep( 1 )
+            time.sleep( 4 )
             pass
             
 
@@ -41,10 +41,10 @@ class TCP:
 
 tcp_over_udp = TCP()
 
-target_ip = "192.168.1.37"
+target_ip = "192.168.1.105"
 while True:
     path = input("EXIT or message:")
     if path == "exit":
         exit(0)
     else:
-        tcp_over_udp.send_file(target_ip, path)
+        tcp_over_udp.sender(target_ip, b'10101' )
