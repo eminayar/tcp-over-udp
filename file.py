deb804b4c76f050c250cddebfc0eb3bf4e335585
eminayar <m.eminayar06@gmail.com>
2019-12-02 00:48:48 +0300

    aegawgawg

--- file.py
+++ file.py
@@ -13,13 +13,13 @@ os.system('clear')
 host_name = socket.gethostname()
 host_ip = "192.168.1.104"
 
-## [<PacketId>,<data>]
+## <PacketId>,<data>
 class TCP:
     def __init__(self):
         self.buffer = queue.Queue(maxsize = WINDOW_SIZE)
         self.sendQueue = queue.Queue(maxsize = WINDOW_SIZE)
         self.packet_id = 0
-        self.ack = [False for i in range(PACKET_ID_CYCLE)]
+        self.ack = [True for i in range(PACKET_ID_CYCLE)]
         _thread.start_new_thread(self.receiver, () )
         _thread.start_new_thread(self.buffer_handler, () )
         _thread.start_new_thread(self.sender, ())
@@ -27,14 +27,14 @@ class TCP:
 
     def persist(self, data):
         time.sleep(1)
-        sendQueue.put(data)
+        self.sendQueue.put(data)
 
     def sender(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         while True:
             ip, pck_id , data = self.sendQueue.get(block=True)
             if not self.ack[pck_id]:
-                print("sending data")
+                print("sending data to", ip)
                 sock.sendto(data, (ip, PORT))
                 _thread.start_new_thread(self.persist, ((ip, pck_id, data), ) )
             
@@ -42,7 +42,10 @@ class TCP:
         while True:
             data = self.buffer.get(block=True)
             print( data.decode('ascii') )
-
+            data = data.decode('ascii').split(",")
+            pck_id = data[0]
+            if data[1] == "ACK":
+                self.ack[pck_id] = True
 
     def receiver(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -62,5 +65,8 @@ while True:
     path = input("EXIT or message:")
     if path == "exit":
         exit(0)
+    elif path == "ack":
+        tcp_over_udp.sendQueue.put((host_ip, 0, b'ACK') )
     else:
+        tcp_over_udp.ack[0] = False
         tcp_over_udp.sendQueue.put((target_ip, 0, b'Hello World!') )
