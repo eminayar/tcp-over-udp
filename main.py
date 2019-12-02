fa8a121d08b7a0c5b8cd222384a1d00c8e5492b5
eminayar <m.eminayar06@gmail.com>
2019-12-02 03:49:29 +0300

    aegawgawg

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ users = {}
 from config import *
 from tcp import TCP
 
-def send_response( host_name, host_ip, target_ip ):
-    import socket
-    response_message = '[' + host_name + ',' + host_ip + ',response]'
+# def send_response( host_name, host_ip, target_ip ):
+#     import socket
+#     response_message = '[' + host_name + ',' + host_ip + ',response]'
 
     # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     #     s.connect((target_ip,12345))
     #     s.sendall(str.encode(response_message))
 
-def send_message( host_name, target_ip, message ):
-    import socket
-    response_message = '[' + host_name + ',' + host_ip + ',message,' + message + ']'
+# def send_message( host_name, target_ip, message ):
+#     import socket
+#     response_message = '[' + host_name + ',' + host_ip + ',message,' + message + ']'
     # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     #     s.connect((target_ip,12345))
     #     s.sendall(str.encode(response_message))
@@ -23,13 +23,14 @@ def announcement_listener( host_name, host_ip ):
     import _thread
     global users
 
+    bufferSize = 1024
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('', UDP_PORT))
+    s.setblocking(False)
     while True:
-        bufferSize = 1024
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.bind(('', UDP_PORT))
-        s.setblocking(False)
         result = select.select([s],[],[])
         msg = result[0][0].recv(bufferSize).decode('ascii')
+        print(msg)
         usr, ip, tp = msg.split(',')
         usr = usr.strip()[1:]
         tp = tp.strip()[:-1]
@@ -37,30 +38,29 @@ def announcement_listener( host_name, host_ip ):
         if tp.strip() == 'announce' and ip != host_ip:
             if (usr not in users) or (usr in users and time.time()-users[usr][1] > 5):
                 users[usr] = (ip,time.time())
-                _thread.start_new_thread( send_response, (host_name, host_ip, users[usr][0], ))
-        s.close()
+                # _thread.start_new_thread( send_response, (host_name, host_ip, users[usr][0], ))
 
-def tcp_listener( host_ip ):
-    import socket
-    import time
-    global users
+# def tcp_listener( host_ip ):
+#     import socket
+#     import time
+#     global users
 
-    while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.bind((host_ip,12345))
-            s.listen(10)
-            conn, addr = s.accept()
-            with conn:
-                # print('Connected by: ', addr)
-                data = conn.recv(1024).decode('ascii').strip()[1:-1].split(',')
-                if len(data) < 3:
-                    print("unsupported message type")
-                elif data[2].strip() == 'response':
-                    if (data[0].strip() not in users) or (data[0].strip() in users and time.time()-users[data[0].strip()][1] > 5):
-                        users[data[0].strip()] = (data[1].strip(),time.time())
-                elif data[2].strip() == 'message':
-                    print(data[0].strip() + ": " + data[3].strip())
+#     while True:
+#         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#             s.bind((host_ip,12345))
+#             s.listen(10)
+#             conn, addr = s.accept()
+#             with conn:
+#                 # print('Connected by: ', addr)
+#                 data = conn.recv(1024).decode('ascii').strip()[1:-1].split(',')
+#                 if len(data) < 3:
+#                     print("unsupported message type")
+#                 elif data[2].strip() == 'response':
+#                     if (data[0].strip() not in users) or (data[0].strip() in users and time.time()-users[data[0].strip()][1] > 5):
+#                         users[data[0].strip()] = (data[1].strip(),time.time())
+#                 elif data[2].strip() == 'message':
+#                     print(data[0].strip() + ": " + data[3].strip())
                 # print(data.decode('ascii'))    
 
 
